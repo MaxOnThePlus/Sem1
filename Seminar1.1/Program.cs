beacd2881f7466e6250dea2c2affc530748806db
Maxim Andreevich <barygin.maksim@yandex.ru>
2022-12-23 00:13:58 +0300

    Сем 1

---
+++ Seminar1.1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int quad = b * b;
+
+// if (quad==a)
+// {
+//     Console.WriteLine($"{a} - является квадратом {b}");
+// }
+// else
+// {
+//     Console.WriteLine($"{a} - не является квадратом {b}");
+// }
+
+// Console.WriteLine ("Введите число");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int LowerBorder = -N;
+
+// while (LowerBorder <= N)
+// {
+//     Console.WriteLine($"{LowerBorder}");
+//     LowerBorder = LowerBorder + 1;
+// }
+
+Console.WriteLine("Введите трехзначное число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int ostatok = x % 10;
+
+Console.WriteLine($"{ostatok}");
---
+++ Seminar1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Console.WriteLine(" Введите число");
+// int x = Convert.ToInt32(Console.ReadLine());
+// x = x * x;
+// Console.WriteLine($"Квадрат вашего числа {x}");
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int quad = b * b;
+if (quad==a)
+{
+    Console.WriteLine($"{a} - является квадратом {b}")
+}
+Сonsole.WriteLine($"{a} - не является квадратом {b}")
